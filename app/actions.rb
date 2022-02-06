8c23408b9e1aee0a98df5e3d38caf767af27d8bf
aimeelien <aimeelien0813@gmail.com>
2022-02-06 00:17:59 +0000

    W3D1

--- app/actions.rb
+++ app/actions.rb
@@ -7,42 +7,45 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-  finstagram_post = {
-    username: "sharky_j",
-    avatar_url: "http://naserca.com/images/sharky_j.jpg",
-    photo_url: "http://naserca.com/images/shark.jpg",
-    time_ago_in_minutes: humanized_time_ago(15),
-    like_count: 0,
+  @finstagram_post_dog = {
+    username: "lovelydog_520",
+    avatar_url: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg",
+    photo_url: "https://www.gannett-cdn.com/media/2021/06/03/USATODAY/usatsports/imageForEntry18-8on.jpg?width=2560",
+    humanized_time_ago: humanized_time_ago(15),
+    like_count: 35,
     comment_count: 1,
     comments: [{
-      username: "sharky_j",
-      text:"Out for the long weekend... too embarrassed to show y'all the beach bod!"
+      username: "lovelydog_520",
+      text:"How lovely dog :D"
       }]
   }
-  finstagram_post_whale = {
-    username: "kirk_whalum",
-    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-    photo_url: "http://naserca.com/images/whale.jpg",
+  @finstagram_post_cat = {
+    username: "kirk_cat",
+    avatar_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUHrmOd3f8vPab0H4Ja15a-pzFczZFJkj5fg&usqp=CAU",
+    photo_url: "https://cdn.theatlantic.com/thumbor/tK3KeCj08mDDu431jVj3ex_GqXc=/0x699:6720x4479/960x540/media/img/mt/2021/05/GettyImages_1218380632/original.jpg",
     humanized_time_ago: humanized_time_ago(65),
-    like_count: 0,
+    like_count: 15,
     comment_count: 1,
     comments: [{
-      username: "kirk_whalum",
-      text: "#weekendvibes"
+      username: "kirk_cat",
+      text: "You have a package!"
     }]
   }
 
-  finstagram_post_marlin = {
-    username: "marlin_peppa",
-    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-    photo_url: "http://naserca.com/images/marlin.jpg",
+  @finstagram_post_pig = {
+    username: "pig_peppa",
+    avatar_url: "https://cdn.vox-cdn.com/thumbor/1JMlykD0FI4RdyHD9EzON04ceBA=/0x0:5000x3200/1400x1050/filters:focal(2652x1434:3452x2234):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/65449715/cute_pig_GettyImages_847218080.0.jpg",
+    photo_url: "https://i.natgeofe.com/k/0ed36c42-672a-425b-9e62-7cc946b98051/pig-fence.jpg",
     humanized_time_ago: humanized_time_ago(190),
-    like_count: 0,
+    like_count: 23,
     comment_count: 1,
     comments: [{
-      username: "marlin_peppa",
+      username: "pig_peppa",
       text: "lunchtime! ;)"
     }]
   }
-  [finstagram_post,finstagram_post_whale,finstagram_post_marlin].to_s
+  @finstagram_posts = [@finstagram_post_dog, @finstagram_post_cat, @finstagram_post_pig]
+  erb(:index)
 end
+
+
